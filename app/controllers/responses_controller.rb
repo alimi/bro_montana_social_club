945b01721e8f9654c01009aad5b61db3f01099b2
Ali Ibrahim <aibrahim2k2@gmail.com>
2014-09-02 22:14:30 -0400

    Make users answer all survey questions

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -2,18 +2,34 @@ class ResponsesController < SurveysController
   def create
     params[:questions].each do |question_id, response_params|
       question = Question.find(question_id)
-      question.responses.create(
-        choice_ids: response_params[:response][:choice_ids],
-        user: current_user
+
+      choice_ids = sanitize_response_params_choice_ids(
+        response_params[:response][:choice_ids]
       )
+
+      if choice_ids.any?
+        question.responses.create(
+          choice_ids: response_params[:response][:choice_ids],
+          user: current_user
+        )
+      end
     end
 
-    flash[:notice] = 'Thanks for completing the survey.'
 
-    if !current_user.paid_annual_dues? && Due.any_for_this_year?
+    if current_user.completed_active_survey?
+      flash[:notice] = 'Thanks for completing the survey.'
       redirect_to new_payment_path
     else
-      redirect_to root_path
+      current_user.responses.for_survey(Survey.active).destroy_all
+      redirect_to survey_path, notice: "Looks like you didn't answer all the " +
+        'questions. Please try again.'
     end
+  end
+
+  private
+
+  def sanitize_response_params_choice_ids(choice_ids)
+    return [choice_ids] if !choice_ids.is_a?(Array)
+    choice_ids.reject { |choice_id| choice_id == '' }
   end
 end
