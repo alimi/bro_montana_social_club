6d22f11786c7609bf049c02418a0535d1bc48887
Ali Ibrahim <aibrahim2k2@gmail.com>
2014-09-03 17:24:56 -0400

    Define ActionMailer default url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'www.bromontana.org' }
+
   # Using Gmail for email delivery
   config.after_initialize do
     config.action_mailer.smtp_settings = {
