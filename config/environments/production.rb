283fdcd496255b94cf4e77cb32328b95fe029736
Ali Ibrahim <aibrahim2k2@gmail.com>
2021-08-14 13:41:45 -0400

    Update SendGrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: 'plain',
-    user_name: Rails.application.secrets.sendgrid_username,
+    user_name: 'apikey',
     password: Rails.application.secrets.sendgrid_password,
     domain: 'heroku.com',
     enable_starttls_auto: true
